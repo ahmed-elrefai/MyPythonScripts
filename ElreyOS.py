52e1cdf4146216ab4235e18c57fbb051a0ea3e05
Ahmed Elrefai <114250600+Abo-hmeed@users.noreply.github.com>
2024-03-31 15:00:28 +0200

    Adding 2 directory functions
    Added the 2 functions:
    - find_dir_type() 
    which returns the type of the majority of the files in the directory passed.
    - is_pure_dir()
    returns whether the dir has only one type of files (excluding sub directories by default) which can be changed from the dirs_included parameter.

--- ElreyOS.py
+++ ElreyOS.py
@@ -14,6 +14,47 @@ def dirs_with_extension(ext) -> list[str]:
     filtered_dirs = [dir for dir in dirs if files_with_extension(ext,dir) is not None]
     return filtered_dirs or None
 
+def find_dir_type(dir_path=".") -> str:
+    contents = os.scandir(dir_path)
+    file_types = {}
+    for obj in contents:
+        if obj.is_file():
+            try:
+                ext = "." + obj.name.split(".")[1]
+            except:
+                if os.access(obj.path, os.X_OK):
+                    ext = "bin"
+                else:
+                    ext = "unknown"
+            finally:
+                try:
+                    file_types[ext] += 1
+                except KeyError:
+                    file_types[ext] = 1
+
+    return max(file_types, key=file_types.get) 
+
+def is_pure_dir(dir_path=".", dirs_included=False) -> bool:
+    contents = os.scandir(dir_path)
+    
+    file_types = set()
+    dir_types = set()
+    
+    for obj in contents:
+        if obj.is_file():
+            try:
+                ext = "." + obj.name.split(".")[-1]
+                file_types.add(ext)
+            except IndexError:
+                continue
+        elif obj.is_dir() and dirs_included:
+            dir_types.add(obj.name)
+    
+    if dirs_included:
+        return len(file_types) == 0 and len(dir_types) == 1
+    else:
+        return len(file_types) == 1
+
 # print("Projects found:")
 # for dir in dirs_with_extension(".sln"):
 #     print(f"- {dir}")
