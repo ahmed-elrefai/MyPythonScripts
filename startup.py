1320259bbcd05df7d861dbe1b534e1534143717f
Ahmed Elrefai <114250600+ahmed-elrefai@users.noreply.github.com>
2024-04-03 17:03:42 +0200

    Update startup.py
    - Implementing an outline for the Organize function and it's other sub functions!
    - plus fully implementing the state function.
    which is used to detect the state of the chosen working directory by recognizing it's file structure.

--- startup.py
+++ startup.py
@@ -1,34 +1,75 @@
 import os
-import ElreyOS
+from ElreyOS import *
 
-projects_file = "~/Mycode/python"
+projects_dir = os.path.expanduser("~/Mycode/python/MyModules") # path relative to current user
+supported_ext = {"python":'.py', "java":".java", "c++":".cpp"}
+
+def organize_dirs():
+    """
+    doing this steps for all project dirs:
+
+    Step1: Choosing one of 3 projects states
+    Step2: Expeling Unrelated content
+    Step3: Redirecting Expeled content
+    Step4: Grouping Projects to Done and InDev
+    Step5: Repeat Each Startup.
+    """
+    state = find_dir_state(projects_dir)
+    print(state)
+    filter_content(state, projects_dir)
 
 
-def clean_system():
-    os.chdir(os.path.expanduser(projects_file))  # Step1: setting clean directory
+
+
+def find_dir_state(working_dir=".") -> int:
+    """returns the state of the working dir, according to file structure
+    if it consists of dirs only:
+        State 1: the working dir has list of projects (dirs that has types)
+        State 2: the working dir has two directories, Done and InDev. (check by names)
+
+    if it consists of files only:
+        State 3: the working dir has different file types
+        returns -1 at empty working directory or single files type (no need to organize)
+        """
+    if find_dir_type(working_dir) == None:
+        content = os.listdir(working_dir)
+        if len(content) == 0:
+            print("Empty Working Directory.")
+            return -1
+        elif len(content) == 2:
+            if ["Done", "InDev"] in content:
+                print("we are in a language Directory.")
+                return 2
+            print("this is an 'InDev' Projects Directory with 2 Projects")
+            return 1
+        else:
+            print("this is an 'InDev' Projects Directory multiple Projects")
+            return 1
+
+    elif find_dir_type(working_dir):
+        print("we found files of type:" + find_dir_type(working_dir))
+        if is_pure_dir(working_dir):
+            if not is_pure_dir(working_dir, True):
+                print("we found subdirs, non-dir files will be considered as In Development")
+                return 3
+            else:
+                print("this is a completely pure directory, nothing to organize!")
+                return -1
+        else:
+            print("found multiple file types!, time to organize!")
+            return 3
+        
     
-    # Step2: look for non-python dirs
-    content = os.scandir(".")
+def filter_content(state:int, dir="."):
+    """filters content in 1 of 3 ways according to the state of file tree"""
     
-    parent_dest_path = os.path.dirname(os.path.expanduser(projects_file))
     
-    for obj in content:
-        if obj.is_dir():
-            dir_type = ElreyOS.find_dir_type(obj.path)
-            
-            if dir_type != ".py":
-                dest_path = os.path.join(parent_dest_path, obj.name)
-                
-                if dir_type == ".cpp":
-                    print(f"- dir: {obj.name}\n of type: '.cpp' does not belong here")
-                
-                elif ElreyOS.is_pure_dir(obj.path):
-                    print(f"- dir: {obj.name}\n of type: '{dir_type}' does not belong here")
-                
+
+
 
 
 def startup():
-    clean_system()
+    organize_dirs()
 
 
 if __name__ == "__main__":
